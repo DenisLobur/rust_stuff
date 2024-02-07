f56740a5118c2f3f0e1b76ae2681d46ae0ce8044
DenysLobur <denismachtalles@gmail.com>
2024-02-07 16:06:38 +0200

    Add parallel mandelbrot

--- mandelbrot/src/main.rs
+++ mandelbrot/src/main.rs
@@ -3,9 +3,55 @@ use num::Complex;
 use image::ColorType;
 use image::png::PNGEncoder;
 use std::fs::File;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+
+    if args.len() != 5 {
+        eprintln!("Usage: {} FILE PIXELS UPPERLEFT LOWERRIGHT",
+                  args[0]);
+        eprintln!("Example: {} mandel.png 1000x750 -1.20,0.35 -1,0.20",
+                  args[0]);
+        std::process::exit(1);
+    }
+
+    let bounds = parse_pair(&args[2], 'x')
+        .expect("error parsing image dimensions");
+    let upper_left = parse_complex(&args[3])
+        .expect("error parsing upper left corner point");
+    let lower_right = parse_complex(&args[4])
+        .expect("error parsing lower right corner point");
+
+    let mut pixels = vec![0; bounds.0 * bounds.1];
+
+    //render(&mut pixels, bounds, upper_left, lower_right);
+    let threads = 8;
+    let rows_per_band = bounds.1 / threads + 1;
+
+    {
+        let bands: Vec<&mut [u8]> =
+            pixels.chunks_mut(rows_per_band * bounds.0).collect();
+        crossbeam::scope(|spawner| {
+            for (i, band) in bands.into_iter().enumerate() {
+                let top = rows_per_band * i;
+                let height = band.len() / bounds.0;
+                let band_bounds = (bounds.0, height);
+                let band_upper_left =
+                    pixel_to_point(bounds, (0, top), upper_left, lower_right);
+                let band_lower_right =
+                    pixel_to_point(bounds, (bounds.0, top + height),
+                                   upper_left, lower_right);
+
+                spawner.spawn(move |_| {
+                    render(band, band_bounds, band_upper_left, band_lower_right);
+                });
+            }
+        }).unwrap();
+    }
+
+    write_image(&args[1], &pixels, bounds)
+        .expect("error writing PNG file");
 }
 
 fn _square_loop(mut x: u64) {
@@ -40,7 +86,7 @@ fn _escape_time(c: Complex<f64>, limit: usize) -> Option<usize> {
     None
 }
 
-fn _parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
+fn parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
     match s.find(separator) {
         None => None,
         Some(index) => {
@@ -54,17 +100,17 @@ fn _parse_pair<T: FromStr>(s: &str, separator: char) -> Option<(T, T)> {
 
 #[test]
 fn test_parse_pair() {
-    assert_eq!(_parse_pair::<i32>("", ','), None);
-    assert_eq!(_parse_pair::<i32>("10", ','), None);
-    assert_eq!(_parse_pair::<i32>(",10", ','), None);
-    assert_eq!(_parse_pair::<i32>("10,20", ','), Some((10, 20)));
-    assert_eq!(_parse_pair::<i32>("10,20xy", ','), None);
-    assert_eq!(_parse_pair::<f64>("0.5x", 'x'), None);
-    assert_eq!(_parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
+    assert_eq!(parse_pair::<i32>("", ','), None);
+    assert_eq!(parse_pair::<i32>("10", ','), None);
+    assert_eq!(parse_pair::<i32>(",10", ','), None);
+    assert_eq!(parse_pair::<i32>("10,20", ','), Some((10, 20)));
+    assert_eq!(parse_pair::<i32>("10,20xy", ','), None);
+    assert_eq!(parse_pair::<f64>("0.5x", 'x'), None);
+    assert_eq!(parse_pair::<f64>("0.5x1.5", 'x'), Some((0.5, 1.5)));
 }
 
-fn _parse_complex(s: &str) -> Option<Complex<f64>> {
-    match _parse_pair(s, ',') {
+fn parse_complex(s: &str) -> Option<Complex<f64>> {
+    match parse_pair(s, ',') {
         Some((re, im)) => Some(Complex { re, im }),
         _ => None
     }
@@ -72,11 +118,11 @@ fn _parse_complex(s: &str) -> Option<Complex<f64>> {
 
 #[test]
 fn test_parse_complex() {
-    assert_eq!(_parse_complex("1.25,-6.23"), Some(Complex { re: 1.25, im: -6.23 }));
-    assert_eq!(_parse_complex(",-0.0625"), None);
+    assert_eq!(parse_complex("1.25,-6.23"), Some(Complex { re: 1.25, im: -6.23 }));
+    assert_eq!(parse_complex(",-0.0625"), None);
 }
 
-fn _pixel_to_point(
+fn pixel_to_point(
     bounds: (usize, usize),
     pixel: (usize, usize),
     upper_left: Complex<f64>,
@@ -92,7 +138,7 @@ fn _pixel_to_point(
 
 #[test]
 fn test_pixel_to_point() {
-    assert_eq!(_pixel_to_point(
+    assert_eq!(pixel_to_point(
         (100, 200),
         (25, 175),
         Complex { re: -1.0, im: 1.0 },
@@ -101,7 +147,7 @@ fn test_pixel_to_point() {
     );
 }
 
-fn _render(pixels: &mut [u8],
+fn render(pixels: &mut [u8],
           bounds: (usize, usize),
           upper_left: Complex<f64>,
           lower_right: Complex<f64>) {
@@ -109,7 +155,7 @@ fn _render(pixels: &mut [u8],
 
     for row in 0..bounds.1 {
         for column in 0..bounds.0 {
-            let point = _pixel_to_point(bounds, (column, row), upper_left, lower_right);
+            let point = pixel_to_point(bounds, (column, row), upper_left, lower_right);
 
             pixels[row * bounds.0 + column] =
                 match _escape_time(point, 255) {
@@ -120,7 +166,7 @@ fn _render(pixels: &mut [u8],
     }
 }
 
-fn _write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
+fn write_image(filename: &str, pixels: &[u8], bounds: (usize, usize)) -> Result<(), std::io::Error> {
     let output = match File::create(filename) {
         Ok(f) => f,
         Err(e) => {
