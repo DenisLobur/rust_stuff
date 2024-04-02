8a3a69fd92e04ae81399f1a647244bf3dab6b122
DenysLobur <denismachtalles@gmail.com>
2024-04-02 19:52:51 +0300

    Add rust book chapter 5.Structs

--- guess-game/src/main.rs
+++ guess-game/src/main.rs
@@ -6,6 +6,23 @@ use colored::Colorize;
 fn main() {
     println!("Guess the number!");
 
+    for number in (1..4).rev() {
+        println!("You have {} guesses left", number);
+    }
+
+    let mut s = String::from("some string");
+    let ss = &s;
+    println!("ss: {}", ss);
+    let sss = &s;
+    println!("sss: {}", sss);
+    let slice = &s[0..4];
+    println!("slice: {}", slice);
+    let first = first_word(&s);
+
+    s.clear();
+    println!("The first word end index is: {}", first);
+    //println!("after The first word end index is: {}", first);
+
     let secret_number = rand::thread_rng().gen_range(1..=100);
     println!("The secret number is: {}", secret_number);
     loop {
@@ -35,4 +52,15 @@ fn main() {
             }
         }
     }
+}
+
+fn first_word(s: &String) -> &str {
+    let bytes = s.as_bytes();
+    for (i, &item) in bytes.iter().enumerate() {
+        if item == b' ' {
+            return &s[..i];
+        }
+    }
+
+    return &s[..];
 }
