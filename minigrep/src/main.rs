584427abef36f6f396701ca10208c96c9a346e24
DenysLobur <denismachtalles@gmail.com>
2024-06-06 18:12:22 +0300

    Redirect errors to a file

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -5,23 +5,23 @@ fn main() {
     let args: Vec<String> = env::args().collect();
 
     let config = minigrep::Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parse args: {err}");
+        eprintln!("Problem parse args: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", config.query);
-    println!("In file {}", config.file_path);
+    // println!("Searching for {}", config.query);
+    // println!("In file {}", config.file_path);
 
     if let Err(e) = minigrep::run(config) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
 
 
-fn parse_config(args: &[String]) -> Config {
-    let query = args[1].clone();
-    let file_path = args[2].clone();
-
-    Config { query, file_path }
-}
+// fn parse_config(args: &[String]) -> Config {
+//     let query = args[1].clone();
+//     let file_path = args[2].clone();
+//
+//     Config { query, file_path }
+// }
