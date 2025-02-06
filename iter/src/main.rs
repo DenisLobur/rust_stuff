73f98bb843d8eaf9eded36f6c71ecabf358c58d0
DenysLobur <denismachtalles@gmail.com>
2025-02-06 23:13:31 +0200

    Wrap up iter project

--- iter/src/main.rs
+++ iter/src/main.rs
@@ -1,15 +1,37 @@
-
 fn print_elements(elements: &[String]) {
     // for element in elements {
     //     println!("{}", element);
     // }
-    elements.iter()
-    .map(|el| format!("{} {}", el, el))
-    .for_each(|el| println!("{}", el));
+    elements
+        .iter()
+        .map(|el| format!("{} {}", el, el))
+        .for_each(|el| println!("{}", el));
 }
 
-fn shorten_strings(elements: &mut Vec<String>) {
-    elements.iter_mut().for_each(|el|el.truncate(1));
+fn shorten_strings(elements: &mut [String]) {
+    elements.iter_mut().for_each(|el| el.truncate(1));
+}
+
+fn to_uppercase(elements: &[String]) -> Vec<String> {
+    elements.iter().map(|el| el.to_uppercase()).collect()
+}
+
+fn move_elements(vec_a: Vec<String>, vec_b: &mut Vec<String>) {
+    vec_a.into_iter().for_each(|el| vec_b.push(el));
+}
+
+fn explode(elements: &[String]) -> Vec<Vec<String>> {
+    elements
+        .iter()
+        .map(|el| el.chars().map(|c| c.to_string()).collect())
+        .collect()
+}
+
+fn find_color_or(elements: &[String], search: &str, fallback: &str) -> String {
+    elements
+        .iter()
+        .find(|el| el.contains(search))
+        .map_or(String::from(fallback), |el| el.to_string())
 }
 
 fn main() {
@@ -20,6 +42,19 @@ fn main() {
     ];
 
     // print_elements(&colors);
-    shorten_strings(&mut colors);
-    println!("{:#?}", colors);
+    // shorten_strings(&mut colors);
+    // println!("{:#?}", colors);
+
+    // let uppercased = to_uppercase(&colors);
+    // println!("{:#?}", uppercased);
+
+    // let mut destination = vec![];
+    // move_elements(colors, &mut destination);
+    // println!("Destination: {:#?}", destination);
+
+    // let exploded = explode(&colors);
+    // println!("{:#?}", exploded);
+
+    let found_color = find_color_or(&colors, "se", "orange");
+    println!("{}", found_color);
 }
