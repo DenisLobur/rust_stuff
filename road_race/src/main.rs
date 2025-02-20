eccc729a1a0f9e8e5b804a0d300b64e1e8b41d5e
DenysLobur <denismachtalles@gmail.com>
2025-02-20 21:55:32 +0200

    Add player movement

--- road_race/src/main.rs
+++ road_race/src/main.rs
@@ -1,5 +1,7 @@
 use rusty_engine::prelude::*;
 
+const PLAYER_SPEED: f32 = 250.0;
+
 #[derive(Resource)]
 struct GameState {
     health_amount: u8,
@@ -9,6 +11,16 @@ struct GameState {
 fn main() {
     let mut game = Game::new();
 
+    // Create the player sprite
+    let player1 = game.add_sprite("player1", SpritePreset::RacingCarBlue);
+    player1.translation.x = -500.0;
+    player1.layer = 10.0;
+    player1.collision = true;
+
+    // Start background music
+    game.audio_manager
+        .play_music(MusicPreset::WhimsicalPopsicle, 0.2);
+
     game.add_logic(game_logic);
     game.run(GameState {
         health_amount: 5,
@@ -17,5 +29,19 @@ fn main() {
 }
 
 fn game_logic(engine: &mut Engine, state: &mut GameState) {
-    println!("Game logic");
+    let mut direction: f32 = 0.0;
+    if engine.keyboard_state.pressed(KeyCode::Up) {
+        direction += 1.0;
+    }
+    if engine.keyboard_state.pressed(KeyCode::Down) {
+        direction -= 1.0;
+    }
+
+    // Move the player sprite
+    let player1 = engine.sprites.get_mut("player1").unwrap();
+    player1.translation.y += direction * PLAYER_SPEED * engine.delta_f32;
+    player1.rotation = direction * 0.15;
+    if player1.translation.y > 360.0 || player1.translation.y < -360.0 {
+        state.health_amount = 0;
+    }
 }
