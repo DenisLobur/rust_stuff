0871deb29538525d9a72963d6060742a38f1501e
DenysLobur <denismachtalles@gmail.com>
2025-03-11 18:32:25 +0200

    Chapter 6.2.2. Make changes to API

--- rust_servers/ezytutors/tutor-db/src/bin/iter5.rs
+++ rust_servers/ezytutors/tutor-db/src/bin/iter5.rs
@@ -1,3 +1,11 @@
+use crate::routes::{course_routes, general_routes};
+use crate::state::AppState;
+use actix_web::{web, App, HttpServer};
+use dotenv::dotenv;
+use sqlx::PgPool;
+use std::sync::Mutex;
+use std::{env, io};
+
 #[path = "../iter5/dbaccess/mod.rs"]
 mod dbaccess;
 #[path = "../iter5/errors.rs"]
@@ -11,6 +19,25 @@ mod routes;
 #[path = "../iter5/state.rs"]
 mod state;
 
-fn main() {
-    println!("Hello, world!");
+#[actix_rt::main]
+async fn main() -> io::Result<()> {
+    dotenv().ok();
+    let database_url = env::var("DATABASE_URL").expect("DATABASE_URL is not set in .env file");
+    let db_pool = PgPool::connect(&database_url).await.unwrap();
+    // Construct App State
+    let shared_data = web::Data::new(AppState {
+        health_check_response: "I'm good. You've already asked me ".to_string(),
+        visit_count: Mutex::new(0),
+        db: db_pool,
+    });
+    //Construct app and configure routes
+    let app = move || {
+        App::new()
+            .app_data(shared_data.clone())
+            .configure(general_routes)
+            .configure(course_routes)
+    };
+    //Start HTTP server
+    let host_port = env::var("HOST_PORT").expect("HOST:PORT address is not set in .env file");
+    HttpServer::new(app).bind(&host_port)?.run().await
 }
