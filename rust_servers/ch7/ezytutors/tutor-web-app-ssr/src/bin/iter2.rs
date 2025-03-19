b34fe8e7ffd386d5bf0f47325ff2c6e143e28f48
DenysLobur <denismachtalles@gmail.com>
2025-03-19 16:46:40 +0200

    Chapter 7.6. Add unit and integration tests for frontend web app

--- rust_servers/ch7/ezytutors/tutor-web-app-ssr/src/bin/iter2.rs
+++ rust_servers/ch7/ezytutors/tutor-web-app-ssr/src/bin/iter2.rs
@@ -58,3 +58,57 @@ fn app_config(config: &mut web::ServiceConfig) {
                 handle_post_tutor)))
     );
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use actix_web::http::{header::CONTENT_TYPE, header::HeaderValue, StatusCode};
+    use actix_web::web::Form;
+
+    #[actix_rt::test]
+    async fn handle_post_1_unit_test() {
+        let params = Form(Tutor {
+            name: "Terry".to_string(),
+        });
+        let tera = Tera::new(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/static/iter2/**/*"
+        ))
+            .unwrap();
+        let webdata_tera = web::Data::new(tera);
+        let resp = handle_post_tutor(
+            webdata_tera, params).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_eq!(
+            resp.headers().get(CONTENT_TYPE).unwrap(),
+            HeaderValue::from_static("text/html")
+        );
+    }
+
+    use actix_web::dev::{Service, ServiceResponse};
+    use actix_web::test::{self, TestRequest};
+
+    // Integration test case
+    #[actix_rt::test]
+    async fn handle_post_1_integration_test() {
+        let tera = Tera::new(concat!(
+        env!("CARGO_MANIFEST_DIR"),
+        "/static/iter2/**/*"
+        ))
+            .unwrap();
+        let mut app = test::init_service(App::new().data(tera).configure(
+            app_config)).await;
+        let req = test::TestRequest::post()
+            .uri("/tutors")
+            .set_form(&Tutor {
+                name: "Terry".to_string(),
+            })
+            .to_request();
+        let resp: ServiceResponse = app.call(req).await.unwrap();
+        assert_eq!(resp.status(), StatusCode::OK);
+        assert_eq!(
+            resp.headers().get(CONTENT_TYPE).unwrap(),
+            HeaderValue::from_static("text/html")
+        );
+    }
+}
